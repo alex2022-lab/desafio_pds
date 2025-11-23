8fa0b5b58c35e9819f05a354b929ed8cf34d1c9e
nicolas arias <n.arias10@ufromail.cl>
2025-11-22 23:09:25 -0300

    feat: add global validation pipe and improve app startup logging

--- ticket_managment/src/main.ts
+++ ticket_managment/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as process from 'node:process';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3102);
+
+  const port = process.env.port ?? 3102;
+  console.log(`App running on port: ${port}`);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+    }),
+  );
+  await app.listen(port);
 }
 bootstrap();

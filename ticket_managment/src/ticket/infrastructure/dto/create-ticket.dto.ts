4b3c813a939366045b10c38ce4186ffccd99eb42
nicolas arias <n.arias10@ufromail.cl>
2025-11-23 23:22:20 -0300

    feat(ticket) enhance ticket DTOs with validation, refactor ticket repository to use axios, and update controller methods

--- ticket_managment/src/ticket/infrastructure/dto/create-ticket.dto.ts
+++ ticket_managment/src/ticket/infrastructure/dto/create-ticket.dto.ts
@@ -1,8 +1,13 @@
 import { TicketType } from '../../domain/enum/ticket.type';
+import { Allow, IsNumber, IsString } from 'class-validator';
 
 export class CreateTicketDto {
+  @IsString()
   eventId: string;
+  @Allow()
   type: TicketType;
+  @IsNumber()
   price: number;
+  @IsNumber()
   availableQuantity: number;
 }
